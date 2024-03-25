98c80c4278e13510491223ba014cce4d0437eb19
Joeadin <Joadinmaina@gmail.com>
2024-03-25 22:00:37 +0300

    commit

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -1,20 +1,28 @@
-"use client";
+'use client'
 
-// Import swiper react components
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
-// Import components
 import CustomButton from "./CustomButton";
 import SwiperNavButtons from "./SwiperNavButtons";
-
 import { motion } from "framer-motion";
 import { fadeIn } from "@/lib/variants";
 
 const HeroSlider = () => {
+  const [typedText, setTypedText] = useState("");
+  const targetText = "Transform Your Life, One Rep at a Time";
+
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      setTypedText((prevText) =>
+        prevText === targetText ? prevText : targetText.substring(0, prevText.length + 1)
+      );
+    }, 100);
+
+    return () => clearInterval(typingInterval);
+  }, [typedText]);
+
   return (
     <Swiper className="h-full">
       <SwiperSlide>
@@ -27,7 +35,7 @@ const HeroSlider = () => {
               viewport={{ once: false, amount: 0.2 }}
               className="h1 text-center lg:text-left nb-2"
             >
-              <span>Transform Your Life,</span> <br />One Rep at a Time
+              <span>{typedText}</span>
             </motion.h1>
             <motion.p
               variants={fadeIn("up", 0.6)}
@@ -36,65 +44,15 @@ const HeroSlider = () => {
               viewport={{ once: false, amount: 0.2 }}
               className="text-white italic text-center lg:text-left nb-4"
             >
-              &quot;The only bad workout is the one that didn&apos;t happen.&quot; - Unknown
+              "The only bad workout is the one that didn't happen." - Unknown
             </motion.p>
-            {/* 
-            <motion.div
-              variants={fadeIn("up", 0.8)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.2 }}
-            >
-              <CustomButton
-                text="Get Started"
-                containerStyles="w-[196px] h-[62px]"
-              />
-            </motion.div>
-            */}
+            
           </div>
         </div>
       </SwiperSlide>
-      <SwiperSlide>
-        <div className="h-full flex justify-end pt-48">
-          <div className="flex flex-col items-center lg:items-start lg:max-w-[700px]">
-            <motion.h1
-              variants={fadeIn("up", 0.4)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.2 }}
-              className="h1 text-center lg:text-left mb-2"
-            >
-              <span>Where sweat</span> <br />meets swagger.
-            </motion.h1>
-            <motion.p
-              variants={fadeIn("up", 0.6)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.2 }}
-              className="text-white italic text-center lg:text-left mb-4"
-            >
-              &quot;Excuses don&apos;t burn calories.&quot; - Unknown
-            </motion.p>
-            <motion.div
-              variants={fadeIn("up", 0.8)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.2 }}
-            >
-              <CustomButton
-                text="Get Started"
-                containerStyles="w-[196px] h-[62px]"
-              />
-            </motion.div>
-          </div>
-        </div>
-      </SwiperSlide>
+      {/* Add other slides */}
       {/* swiper nav buttons */}
-      <SwiperNavButtons
-        containerStyles="absolute bottom-2 lg:bottom-0 right-0 h-[130px] w-full lg:w-[700px] z-50 flex justify-center lg:justify-start gap-1"
-        btnStyles="border border-accent text-white w-[56px] h-[56px] flex justify-center items-center hover:bg-accent transition-all duration-300"
-        iconStyles="text-2xl"
-      />
+  
     </Swiper>
   );
 };
